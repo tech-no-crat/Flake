551e747f5f4706a92208b26da00e95f9bbb6a919
Tech-n0-crat <tech-no-crat2526@gmail.com>
2025-12-28 20:52:09 -0500

    updated some items

--- home/surface-book-active/home.nix
+++ home/surface-book-active/home.nix
@@ -5,25 +5,17 @@
   home.homeDirectory = "/home/shyam";
 
   # --- User Packages ---
-  home.packages = with pkgs; [
-    # Communication / Sync
-    nextcloud-client
-    moonlight-qt
+  home.packages = [
+    pkgs.nextcloud-client
+    pkgs.moonlight-qt
+    pkgs.obsidian
+    pkgs.neovim
+    pkgs.nodejs_24
+    pkgs._1password-cli
     
-    # Productivity
-    obsidian
-    
-    # Development / Tools
-    neovim
-    nodejs_24
-    #n8n
-    
-    # CLI Tools
-    _1password-cli
-    
-  ]; ++ (with pkgs-unstable; [
-    heroic-launcher
-  ]);
+    # Explicitly from unstable
+    pkgs-unstable.heroic-launcher
+  ];
   # Allow unfree packages (just in case)
   nixpkgs.config.allowUnfree = true;
 
