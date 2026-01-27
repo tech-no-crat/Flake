10c3c9265e60df0c6ed115425d84a89b658f837c
shyam <shuklashyam@outlook.com>
2026-01-26 20:01:54 -0500

    syntax

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -18,14 +18,13 @@
     (pkgs-unstable.heroic.override {
       extraPkgs = p: [ p.gamescope p.mangohud p.gamemode ];
     }
+    )
     pkgs-unstable.wineWowPackages.stable # Essential for 32-bit & 64-bit games
     pkgs-unstable.winetricks
     pkgs-unstable.protonup-qt     # To download GE-Proton for Heroic/Steam
     pkgs-unstable.nexusmods-app
     pkgs-unstable.protontricks
-    
-    )
-    
+        
     # Wine & Support Tools
 
   ];
