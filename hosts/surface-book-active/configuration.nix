9386ba293bf8bc2bd82b0e2e2d6c065d8b9a192a
Tech-n0-crat <tech-no-crat2526@gmail.com>
2026-02-08 14:48:36 -0500

    test surface kernel

--- hosts/surface-book-active/configuration.nix
+++ hosts/surface-book-active/configuration.nix
@@ -10,7 +10,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   networking.hostName = "surface-book-active";
-  hardware.microsoft-surface.kernelVersion = “stable”
+  hardware.microsoft-surface.kernelVersion = “stable”;
   
   hardware.graphics = {
     enable = true;
