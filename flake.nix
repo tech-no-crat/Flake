3e88e3faefb56674fb5f90afcdd8aeb0045c97b6
Tech-n0-crat <tech-no-crat2526@gmail.com>
2026-02-08 13:23:05 -0500

    swap to unstable attempt 2

--- flake.nix
+++ flake.nix
@@ -10,7 +10,9 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # --- NEW: Hardware support for Surface devices ---
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    #nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    # Fix for 25.11
+    nixos-hardware.url = "github:8bitbuddhist/nixos-hardware?ref=surface-rust-target-spec-fix";
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nixos-hardware, ... }@inputs: 
