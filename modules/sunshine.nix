903e41a97a5269822522c61b8d05cb84e4764f71
shyam <shuklashyam@outlook.com>
2026-03-20 12:59:42 -0400

    fix sunshine module

--- modules/sunshine.nix
+++ modules/sunshine.nix
@@ -1,6 +1,6 @@
 { config, pkgs, pkgs-unstable, ... }:
 
-{
+
 {
   # 1. Force the service to use the unstable version
   services.sunshine = {
@@ -25,7 +25,7 @@
       ];
     };
   };
-}
+
   # 2. Add the unstable package to your system environment for manual CLI access
   environment.systemPackages = [
     pkgs-unstable.sunshine
